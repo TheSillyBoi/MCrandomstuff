b1ec3c3f9ae67c0169789e79df891524477a3d94
Adrian Tennies <adrian@tennies.us>
2025-08-04 21:52:03 -0500

    Initial Commit trying to Fix stuff

---
+++ src/client/java/adrian/randomstuff/RandomstuffClient.java
@@ -0,0 +1,10 @@
+package adrian.randomstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RandomstuffClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
