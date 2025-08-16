59ab7f2ec81e7d94dd5b0babcf35f0fafa5a09df
Adrian Tennies <adrian@tennies.us>
2025-08-16 14:06:24 -0500

    Updating, adding another item

--- src/main/java/adrian/randomstuff/item/ModItems.java
+++ src/main/java/adrian/randomstuff/item/ModItems.java
@@ -4,40 +4,50 @@ import adrian.randomstuff.Randomstuff;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.Identifier;
-import org.spongepowered.include.com.google.common.base.Function;
+import net.minecraft.util.Rarity;
 
-import static net.minecraft.item.Items.register;
+import java.util.function.Function;
 
 public class ModItems {
     public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
         // Create the item key.
         RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Randomstuff.MOD_ID, name));
-
         // Create the item instance.
         Item item = itemFactory.apply(settings.registryKey(itemKey));
-
         // Register the item.
         Registry.register(Registries.ITEM, itemKey, item);
-
         return item;
     }
+
     public static final Item hatchet;
 
     static {
         hatchet = register("hatchet", Item::new, new Item.Settings());
     }
 
+    public static final Item lightsaber = register(
+            "lightsaber",
+            settings -> new Item(
+                    settings.sword(ToolMaterial.WOOD,
+                            3.0f,
+                            -2.4f)
+            ),
+            new Item.Settings()
+    );
+
     public static void initialize() {
-        // Get the event for modifying entries in the ingredients group.
-        // And register an event handler that adds our suspicious item to the ingredients group.
-
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
-                .register((itemGroup) -> itemGroup.add(ModItems.hatchet));
+                .register(group -> {
+                    group.add(ModItems.hatchet);
+                    group.add(ModItems.lightsaber);
+                });
     }
-
 }
